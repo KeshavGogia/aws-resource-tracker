d6e5b022b08d951ca5126c1571a3be228490137c
Keshav <keshavgogia57@gmail.com>
2024-02-11 15:28:28 +0530

    Create list-users.sh

---
+++ git-api-shell-script/list-users.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#########################
+# About : Script to display all the user's with pull access to a GitHub Repo
+# Input : This script inputs username and github api token
+# Version : v1
+# Date  : 10 Feb 2024
+# Author : Keshav
+#########################
+
+
+# Calling the helper function first
+
+helper
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+# If user has not provided with sufficient cmd args then helper function is called
+
+helper() {
+    required_cmd_args=2
+    if [ $# -ne $required_cmd_args ]; then
+        echo "Provide completed command-line arguments"
+        exit 1
+    fi
+}
+
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
